932f8ed5c0ccbda33817e890af8ed3c44b09e605
Adrian Furman <fadikk367@gmail.com>
2020-11-05 16:40:19 +0100

    Final HttpRestRequest refactor

--- src/main/java/edu/agh/wfiis/solid/srp/example1/HttpRestRequest.java
+++ src/main/java/edu/agh/wfiis/solid/srp/example1/HttpRestRequest.java
@@ -11,19 +11,23 @@ import java.util.List;
 
 public class HttpRestRequest {
     protected MuleMessage muleMessage;
+    
+    @Deprecated
     protected Constraints validationConstraints;
 
     public HttpRestRequest(MuleMessage muleMessage) {
         this.muleMessage = muleMessage;
     }
 
+    @Deprecated
     public MuleMessage validate(Constraints validationConstraints) throws InvalidHeaderException {
         this.validationConstraints = validationConstraints;
         List<Constraint> headerConstraints = validationConstraints.getHeaderConstraints();
 
-        List<HeaderValidationError> headerValidationErrors = validateHeaders(headerConstraints);
-        if (!headerValidationErrors.isEmpty()) {
-            throw new InvalidHeaderException(headerValidationErrors.get(0).getError());
+        ValidationResult headersValidationResult = validateHeaders(headerConstraints);
+        if (!headersValidationResult.isValid()) {
+            HeaderValidationError firstValidationError = headersValidationResult.getValidationErrors().get(0);
+            throw new InvalidHeaderException(firstValidationError.getErrorMessage());
         }
 
         setMissingHeadersDefaultValues(headerConstraints);
@@ -31,26 +35,26 @@ public class HttpRestRequest {
         return muleMessage;
     }
 
-    private List<HeaderValidationError> validateHeaders(List<Constraint> headerConstraints) {
-        List<HeaderValidationError> errorMessages = new ArrayList<>();
+    public ValidationResult<HeaderValidationError> validateHeaders(List<Constraint> headerConstraints) {
+        ValidationResult<HeaderValidationError> validationResult = new ValidationResult<HeaderValidationError>();
 
         for (Constraint constraint : headerConstraints) {
             String headerName = constraint.getHeaderName();
             String headerValue = muleMessage.getHeader(headerName);
 
             if (headerValue == null && constraint.isHeaderRequired()) {
-                errorMessages.add(new MissingHeaderValidationError(headerName));
+                validationResult.addError(new MissingHeaderValidationError(headerName));
             }
 
             if (headerValue != null && !constraint.validate(headerValue)) {
-                errorMessages.add(new InvalidHeaderValueValidationError(headerName, headerValue));
+                validationResult.addError(new InvalidHeaderValueValidationError(headerName, headerValue));
             }
         }
 
-        return errorMessages;
+        return validationResult;
     }
 
-    private void setMissingHeadersDefaultValues(List<Constraint> headerConstraints) {
+    public void setMissingHeadersDefaultValues(List<Constraint> headerConstraints) {
         for (Constraint constraint : headerConstraints) {
             String headerName = constraint.getHeaderName();
             String headerDefaultValue = constraint.getDefaultValue();
@@ -61,34 +65,61 @@ public class HttpRestRequest {
     }
 }
 
-interface HeaderValidationError {
-    String getError();
+class ValidationResult<T> {
+    private List<T> errors = new ArrayList<T>();
+
+    ValidationResult() {}
+
+    public List<T> getValidationErrors() {
+        return this.errors;
+    }
+
+    public addError(T error) {
+        this.errors.add(error);
+    }
+
+    public isValid() {
+        return this.errors.isEmpy();
+    }
 }
 
-class MissingHeaderValidationError  implements HeaderValidationError {
-    private final String headerName;
 
-    public MissingHeaderValidationError(String headerName) {
+abstract class HeaderValidationError {
+    protected final String headerName;
+    
+    HeaderValidationError(String headerName) {
         this.headerName = headerName;
+    }
+    public getHeaderName() {
+        return this.headerName;
+    }
+
+    abstract String getErrorMessage();
+}
+
+
+class MissingHeaderValidationError extends HeaderValidationError {
+    public MissingHeaderValidationError(String headerName) {
+        super(headerName);
     }
 
     @Override
-    public String getError() {
+    public String getErrorMessage() {
         return "Required header " + headerName + " not specified";
     }
 }
 
-class InvalidHeaderValueValidationError  implements HeaderValidationError {
-    private final String headerName;
+
+class InvalidHeaderValueValidationError extends HeaderValidationError {
     private final String headerValue;
 
     public InvalidHeaderValueValidationError(String headerName, String headerValue) {
-        this.headerName = headerName;
+        super(headerName);
         this.headerValue = headerValue;
     }
 
     @Override
-    public String getError() {
+    public String getErrorMessage() {
         return MessageFormat.format("Invalid value {0} format for header {1}.", headerValue, headerName);
     }
 }
