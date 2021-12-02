0d3e9e729c5aaa2000daf511d07af5fd47359d06
dtrybuch <dominik.trybuch@gmail.com>
2021-12-02 09:13:00 +0100

    refactor to srp

--- src/main/java/edu/agh/wfiis/solid/srp/example1/HttpRestRequest.java
+++ src/main/java/edu/agh/wfiis/solid/srp/example1/HttpRestRequest.java
@@ -10,36 +10,38 @@ import java.text.MessageFormat;
 public class HttpRestRequest {
 
     protected MuleMessage muleMessage;
-    protected Constraints validationConstraints;
 
     public HttpRestRequest(MuleMessage muleMessage) {
         this.muleMessage = muleMessage;
     }
 
     public MuleMessage validate(Constraints validationConstraints) throws InvalidHeaderException {
-        this.validationConstraints = validationConstraints;
-        processHeaders();
-        return muleMessage;
-    }
-
-    private void processHeaders() throws InvalidHeaderException {
         for (Constraint constraint : validationConstraints.getHeaderConstraints()) {
             String headerName = constraint.getHeaderName();
             String headerValue = muleMessage.getHeader(headerName);
 
-            if (headerValue == null && constraint.isHeaderRequired()) {
+            validateHeader(constraint, headerName, headerValue);
+            setMissingHeaderValueToDefault(constraint, headerName, headerValue);
+        }
+        return muleMessage;
+    }
+
+    private void validateHeader(Constraint constraint, String headerName, String headerValue) throws InvalidHeaderException {
+        if (headerValue == null) {
+            if (constraint.isHeaderRequired()) {
                 throw new InvalidHeaderException("Required header " + headerName + " not specified");
             }
-
-            if (headerValue == null && constraint.getDefaultValue() != null) {
-                muleMessage.setHeader(headerName, constraint.getDefaultValue());
-            }
-
-            if (headerValue != null) {
-                if (!constraint.validate(headerValue)) {
-                    throw new InvalidHeaderException(MessageFormat.format("Invalid value format for header {0}.", headerName));
-                }
+        } else {
+            if (!constraint.validate(headerValue)) {
+                throw new InvalidHeaderException(MessageFormat.format("Invalid value format for header {0}.", headerName));
             }
         }
     }
+
+    private void setMissingHeaderValueToDefault(Constraint constraint, String headerName, String headerValue) {
+        if (headerValue == null && constraint.getDefaultValue() != null) {
+            muleMessage.setHeader(headerName, constraint.getDefaultValue());
+        }
+    }
+
 }
